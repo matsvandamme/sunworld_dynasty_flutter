0adfec8ca4732254ea74a0501d727aa2a48b36db
Matthieu Van Damme <matsvandamme@gmail.com>
2020-07-11 21:06:00 +0200

    Page indicators added

--- lib/page_modules/page_view.dart
+++ lib/page_modules/page_view.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:roomserviceapp/external_packages/utils.dart';
 import 'package:roomserviceapp/pages/dessert_menu.dart';
 import 'package:roomserviceapp/pages/drinks_menu.dart';
 import 'package:roomserviceapp/pages/food_menu.dart';
 import 'package:page_view_indicators/circle_page_indicator.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 var _currentPageNotifier = ValueNotifier<int>(0);
+final controller = PageController(
+  initialPage: 0,
+);
+var scrollDirection = Axis.horizontal;
+var actionIcon = Icons.swap_vert;
 
 class PageViewWidget extends StatefulWidget {
   @override
@@ -17,17 +24,13 @@ class _PageViewWidgetState extends State<PageViewWidget> {
       GlobalKey<ScaffoldState>();
 
   ///Page Controller for the PageView
-  final controller = PageController(
-    initialPage: 0,
-  );
-  var scrollDirection = Axis.horizontal;
-  var actionIcon = Icons.swap_vert;
 
   @override
   Widget build(BuildContext context) {
     Size _screenSize = MediaQuery.of(context).size;
     return Scaffold(
       key: _scaffoldKey,
+
       ///A Page View with 3 children
       body: PageView(
         onPageChanged: (int index) {
@@ -36,6 +39,7 @@ class _PageViewWidgetState extends State<PageViewWidget> {
         physics: BouncingScrollPhysics(),
         controller: controller,
         scrollDirection: scrollDirection,
+
         ///Enable physics property to provide your PageView with a
         ///custom scroll behaviour
         ///Here BouncingScrollPhysics will pull back the boundary
@@ -88,18 +92,25 @@ class PageIndicator extends StatefulWidget {
 }
 
 class _PageIndicatorState extends State<PageIndicator> {
-
   @override
   Widget build(BuildContext context) {
     return Positioned(
       left: 0.0,
       right: 0.0,
-      bottom: 0.0,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: CirclePageIndicator(
-          itemCount: 3,
-          currentPageNotifier: _currentPageNotifier,
+      bottom: 15.0,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SmoothPageIndicator(
+            controller: controller, // PageController
+            count: 3,
+            effect: WormEffect(
+              activeDotColor: Color(0xff780003),
+              dotColor: Colors.white,
+              paintStyle: PaintingStyle.stroke,
+              strokeWidth: 1,
+            ), // your preferred effect
+          ),
         ),
       ),
     );
