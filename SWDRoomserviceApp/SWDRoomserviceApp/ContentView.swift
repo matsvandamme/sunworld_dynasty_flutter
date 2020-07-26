abbda76f3c26b7180e786ca2b33f44e6a6f179c3
matthieuvandamme <matsvandamme@gmail.com>
2020-07-26 05:46:32 +0200

    ok

---
+++ SWDRoomserviceApp/SWDRoomserviceApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SWDRoomserviceApp
+//
+//  Created by Matthieu Van Damme on 26/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SWDRoomserviceApp/SWDRoomserviceApp/SWDRoomserviceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWDRoomserviceAppApp.swift
+//  SWDRoomserviceApp
+//
+//  Created by Matthieu Van Damme on 26/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct SWDRoomserviceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
